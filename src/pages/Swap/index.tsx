a4b7809709dca6d6286485b0534d4af79e20d8b6
tunogya <tunogya@qq.com>
2021-09-17 17:09:50 +0800

    fix: delete Swap index's if (ETH-USDT)

--- src/pages/Swap/index.tsx
+++ src/pages/Swap/index.tsx
@@ -12,7 +12,7 @@ import TokenInput from 'src/components/TokenInput'
 import useSwap from 'src/libs/web3/hooks/useSwap'
 import { TransactionType } from 'src/libs/web3/hooks/useTransaction'
 import useWeb3 from 'src/libs/web3/hooks/useWeb3'
-import {formatETH, formatUSDT, toBigNumber} from 'src/libs/web3/util'
+import {toBigNumber} from 'src/libs/web3/util'
 
 import { RiskAction, useRiskModal } from '../shared/RiskModal'
 import TransactionButtonGroup from '../shared/TransactionButtonGroup'
@@ -61,49 +61,21 @@ const Swap: FC = () => {
       const curVer = ++ver
       setChange('src')
 
-      if (src.symbol === "ETH" && dest.symbol === "USDT"){
-        const info = await api?.Contracts.UniswapQuoter.quoteExactInputSingle("0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2", "0xdAC17F958D2ee523a2206206994597C13D831ec7", api.Tokens.ETH.parse(amount).toFixed(0))
-        if (ver > curVer) {
-          return
-        }
-        if (info && amount) {
-          pair.dest = {
-            symbol: pair.dest.symbol,
-            amount: (formatUSDT(info)).toFixed(Math.min(api?.Tokens[pair.dest.symbol].decimals || 18, 8))
-          }
-        }
-        setPair({ ...pair })
-
-      }else if (src.symbol === "USDT" && dest.symbol === "ETH") {
-        const info = await api?.Contracts.UniswapQuoter.quoteExactInputSingle("0xdAC17F958D2ee523a2206206994597C13D831ec7", "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2", api.Tokens.USDT.parse(amount).toFixed(0))
-        if (ver > curVer) {
-          return
-        }
-        if (info && amount) {
-          pair.dest = {
-            symbol: pair.dest.symbol,
-            amount: (formatETH(info)).toFixed(Math.min(api?.Tokens[pair.dest.symbol].decimals || 18, 8))
-          }
-        }
-        setPair({ ...pair })
-
-      } else{
-        const info = await api?.getSwapInfo(symbol, pair.dest.symbol, amount)
-        if (ver > curVer) {
-          return
-        }
-
-        if (info && amount) {
-          const ratio = info.amountOut.div(amount)
-          pair.dest = {
-            symbol: pair.dest.symbol,
-            amount: toBigNumber(amount)
-              .multipliedBy(ratio)
-              .toFixed(Math.min(api?.Tokens[pair.dest.symbol].decimals || 18, 8)),
-          }
-        }
-        setPair({ ...pair })
+      const info = await api?.getSwapInfo(symbol, pair.dest.symbol, amount)
+      if (ver > curVer) {
+        return
       }
+
+      if (info && amount) {
+        const ratio = info.amountOut.div(amount)
+        pair.dest = {
+          symbol: pair.dest.symbol,
+          amount: toBigNumber(amount)
+            .multipliedBy(ratio)
+            .toFixed(Math.min(api?.Tokens[pair.dest.symbol].decimals || 18, 8)),
+        }
+      }
+      setPair({ ...pair })
     } else {
       if (symbol === pair.dest.symbol && amount === pair.dest.amount) {
         return
@@ -125,48 +97,22 @@ const Swap: FC = () => {
       const curVer = ++ver
       setChange('dest')
 
-      if (src.symbol === "ETH" && dest.symbol === "USDT"){
-        const info = await api?.Contracts.UniswapQuoter.quoteExactOutputSingle("0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2", "0xdAC17F958D2ee523a2206206994597C13D831ec7", api.Tokens.USDT.parse(amount).toFixed(0))
-        if (ver > curVer) {
-          return
-        }
-        if (info && amount) {
-          pair.src = {
-            symbol: pair.src.symbol,
-            amount: (formatETH(info)).toFixed(Math.min(api?.Tokens[pair.src.symbol].decimals || 18, 8))
-          }
-        }
-        setPair({ ...pair })
-      }else if(src.symbol === "USDT" && dest.symbol === "ETH"){
-        const info = await api?.Contracts.UniswapQuoter.quoteExactOutputSingle("0xdAC17F958D2ee523a2206206994597C13D831ec7", "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2", api.Tokens.ETH.parse(amount).toFixed(0))
-        if (ver > curVer) {
-          return
-        }
-        if (info && amount) {
-          pair.src = {
-            symbol: pair.src.symbol,
-            amount: (formatUSDT(info)).toFixed(Math.min(api?.Tokens[pair.src.symbol].decimals || 18, 8))
-          }
-        }
-        setPair({ ...pair })
-      } else{
-        const info = await api?.getSwapInfo(pair.src.symbol, symbol, "1")
-        if (ver > curVer) {
-          return
-        }
-
-        if (info && amount) {
-          // const ratio = info.amountOut.div(amount)
-          pair.src = {
-            symbol: pair.src.symbol,
-            amount: toBigNumber(amount)
-              .div(info.amountOut)
-              .toFixed(Math.min(api?.Tokens[pair.src.symbol].decimals || 18, 8)),
-          }
-        }
-
-        setPair({ ...pair })
+      const info = await api?.getSwapInfo(pair.src.symbol, symbol, "1")
+      if (ver > curVer) {
+        return
       }
+
+      if (info && amount) {
+        // const ratio = info.amountOut.div(amount)
+        pair.src = {
+          symbol: pair.src.symbol,
+          amount: toBigNumber(amount)
+            .div(info.amountOut)
+            .toFixed(Math.min(api?.Tokens[pair.src.symbol].decimals || 18, 8)),
+        }
+      }
+
+      setPair({ ...pair })
     }
   }
 
